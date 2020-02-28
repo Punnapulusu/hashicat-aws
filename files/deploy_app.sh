a6c3d73e7cb9d1212f4e9680b53e7c8fd58e98bd
Punna Reddy <punnarpulusu@gmail.com>
2020-02-28 21:25:15 +0000

    this is first commit

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to Earnin's app. Replace this text with your own.
+  Welcome to Earnin's app on training. Replace this text with your own.
   <!-- END -->
 
   </div>
